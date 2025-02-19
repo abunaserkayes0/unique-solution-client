055be7232b6186c2f05fe954b4cc54ab7dbdbcec
abunaserkayes0 <abunaserkayes0@gmail.com>
2025-02-19 13:49:03 +0600

    fixed:navitems

--- src/components/ui/navbar/navItems.jsx
+++ src/components/ui/navbar/navItems.jsx
@@ -1,79 +1,121 @@
 import { Link } from "react-router-dom";
 
 export const navItems = (
-  <>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">HomePage</Link>
+  <ul className="flex flex-col md:flex-row gap-4 text-xs font-semibold md:text-lg">
+    <li>
+      <Link to="/">HomePage</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
+    <li>
       <div className="dropdown dropdown-hover dropdown-right lg:dropdown-bottom">
-        <div tabIndex={0}>Service</div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content menu bg-base-100 rounded-box z-50 w-80 p-2 shadow-sm"
-        >
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Procurement & Supply Chain Consultancy</Link>
+        <li tabIndex={0} className="focus:outline-none">
+          Service
+        </li>
+        <ul className="dropdown-content menu bg-base-100 rounded-box z-50 w-40 p-2 shadow-sm">
+          <li className="relative group">
+            <Link to="/services/consultancy">Consultancy</Link>
+            <ul className="absolute left-full top-0 hidden group-hover:block bg-base-100 rounded-box z-50 w-64 p-2 shadow-sm">
+              <li>
+                <Link to="/services/consultancy/business">
+                  Procurement & Supply Chain Consultancy
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/technology">
+                  Business Establishment & Investment Advisory
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Real Estate & Infrastructure Advisory
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Textile & Apparel Industry Consultancy
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Logistics & Transportation Advisory
+                </Link>
+              </li>
+            </ul>
           </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Business Startup & Investment Consultancy</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Real Estate & Infrastructure Consultancy</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Health & Wellness Consultancy</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Garments & Textiles Consultancy</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Agriculture & Feed Raw Materials</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Organic Foods & Fruits</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Transportation & Logistics Consultancy</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Construction & Engineering Services</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Education & Skill Development</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Unique Wood Design (Furniture & Interior)</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Eyewear & Optical Business</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Photography & Event Management</Link>
-          </li>
-          <li className="text-xs font-semibold md:text-lg">
-            <Link to="">Digital Services</Link>
+          <li className="relative group">
+            <Link to="/services/consultancy">Advisory Services</Link>
+            <ul className="absolute left-full top-0 hidden group-hover:block bg-base-100 rounded-box z-50 w-64 p-2 shadow-sm">
+              <li>
+                <Link to="/services/consultancy/business">
+                  Healthcare & Wellness Solutions
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/technology">
+                  Education & Skill Development
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Real Estate Development & Property Management
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Agricultural Commodities & Feed Solutions
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Organic & Specialty Food Distribution
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/technology">
+                  Integrated Transportation & Logistics Solutions
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Furniture Manufacturing & Interior Design Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Optical & Vision Care Solutions
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/technology">
+                  Event Planning, Photography & Tourism Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/services/consultancy/finance">
+                  Digital Innovation & Technology Solutions
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">About Us</Link>
+    <li>
+      <Link to="/about">About Us</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">Affiliate Program</Link>
+    <li>
+      <Link to="/affiliate">Join Our Affiliate Program</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">Blog</Link>
+    <li>
+      <Link to="/blog">Blog</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">Contact Us</Link>
+    <li>
+      <Link to="/contact">Contact Us</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">Testimonial</Link>
+    <li>
+      <Link to="/testimonial">Testimonial</Link>
     </li>
-    <li className="text-xs font-semibold md:text-lg">
-      <Link to="">FAQ</Link>
+    <li>
+      <Link to="/faq">FAQ</Link>
     </li>
-  </>
+  </ul>
 );
