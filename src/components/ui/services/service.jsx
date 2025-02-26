e832ea35e590aadb3d5f56863b41485f75dbff1f
abunaserkayes0 <abunaserkayes0@gmail.com>
2025-02-26 18:04:22 +0600

    fixed:some issue of api

--- src/components/ui/services/service.jsx
+++ src/components/ui/services/service.jsx
@@ -16,10 +16,16 @@ export default function Service({
           alt={title}
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 transition-opacity duration-500 group-hover:opacity-70"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-60 transition-opacity duration-500 lg:opacity-0 group-hover:opacity-70"></div>
       </figure>
 
-      <div className="absolute inset-0 flex flex-col justify-end p-4 text-white opacity-0 translate-y-8 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0">
+      <div
+        className="absolute inset-0 flex flex-col justify-end p-4 text-white 
+        opacity-100 translate-y-0 
+        sm:opacity-100 sm:translate-y-0  
+        md:opacity-100 md:translate-y-0 
+        lg:opacity-0 lg:translate-y-8 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0"
+      >
         <h2 className="text-2xl font-bold">{slogan}</h2>
         <p className="text-sm">{shortmessage}</p>
       </div>
